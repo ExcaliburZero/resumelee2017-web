f3cd9bfde29aa1cf205db4cc8f48a54727606a9d
Christopher Wells <cwellsny@nycap.rr.com>
2017-02-25 13:29:04 -0500

    Implement dir and prompt

--- src/Main.purs
+++ src/Main.purs
@@ -5,13 +5,16 @@ import Prelude hiding (append)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Control.Monad.Eff.JQuery (JQuery, JQueryEvent, on, append, css, create, appendText, body, ready, setText, getValue, select, getProp, setAttr, attr, setProp, remove)
+import Control.Monad.Eff.Ref (newRef, REF, readRef)
 import Control.Monad.Except (runExcept)
 import Data.Foreign.Class (read)
 import Data.Foldable (for_)
+import Data.String.Utils (words)
 import DOM (DOM)
 import Partial.Unsafe (unsafePartial)
 
-main :: forall eff. Eff ( dom :: DOM
+main :: forall eff. Eff ( ref :: REF
+                        , dom :: DOM
                         , console :: CONSOLE
                         | eff
                         ) Unit
@@ -21,35 +24,45 @@ main = ready $ do
     log "Script imported"
 
     cmdForm <- select "#commandForm"
+    prompt <- select "#prompt"
     input  <- select "#command"
     output <- select "#output"
 
-    on "change" (handleCommand input output) cmdForm
+    let dir = "~/"
+    curDir <- newRef dir
+    setText (dir <> promptSym) prompt
+
+    on "change" (handleCommand prompt input output curDir) cmdForm
   where
-    handleCommand :: JQuery
-      -> JQuery
-      -> JQueryEvent
-      -> JQuery
-      -> Eff ( dom :: DOM
-             , console :: CONSOLE
-             | eff
-             ) Unit
-    handleCommand input output _ _ = unsafePartial do
+--    handleCommand :: JQuery
+--      -> JQuery
+--      -> JQueryEvent
+--      -> JQuery
+--      -> Eff ( dom :: DOM
+--             , console :: CONSOLE
+--             | eff
+--             ) Unit
+    handleCommand prompt input output curDir _ _ = unsafePartial do
       val <- getValue input
       for_ (runExcept (read val)) \command -> do
         outLine <- create "<p>"
-        setText ("~> " <> command) outLine
+        dir <- readRef curDir
+        setText (dir <> promptSym <> command) outLine
+        setText (dir <> promptSym) prompt
         append outLine output
         setProp "value" "" input
+        runCommand command input output
         scrollVal <- getProp "scrollHeight" output
         for_ (runExcept (read scrollVal)) \(scrollH :: Int) -> do
           setProp "scrollTop" scrollH output
-        runCommand command input output
-    runCommand comm input output = case comm of
-      "clear" -> do
+    runCommand comm input output = case words comm of
+      ["clear"] -> do
         outLines <- select "#output p"
         remove outLines
       _ -> do
         outLine <- create "<p>"
         setText ("Invalid command: " <> comm) outLine
         append outLine output
+
+promptSym :: String
+promptSym = " $ "
