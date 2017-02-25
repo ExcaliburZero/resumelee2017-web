b22445ba5c3b734f91f1b05065571a150f3279ce
Christopher Wells <cwellsny@nycap.rr.com>
2017-02-25 00:25:34 -0500

    Start working on terminal functionality

--- src/Main.purs
+++ src/Main.purs
@@ -1,9 +1,51 @@
 module Main where
 
-import Prelude
+import Prelude hiding (append)
+
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Eff.JQuery (JQuery, JQueryEvent, on, append, css, create, appendText, body, ready, setText, getValue, select)
+import Control.Monad.Except (runExcept)
+import Data.Foreign.Class (read)
+import Data.Foldable (for_)
+import DOM (DOM)
+import Partial.Unsafe (unsafePartial)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = do
-  log "Hello sailor!"
+main :: forall eff. Eff ( dom :: DOM
+                        , console :: CONSOLE
+                        | eff
+                        ) Unit
+main = ready $ do
+    body <- body
+
+    log "Script imported"
+
+    input <- select "#command"
+  
+    output <- select "#output"
+    --outLine <- create "<p>"
+    --append outLine output
+
+    --setText "Terminal opened ..." outLine
+  
+
+  --div <- create "<div>"
+  --appendText "Your Name: " div
+  --append div body
+  --log "Hi"
+    on "change" (handleCommand input output) input
+  where
+    handleCommand :: JQuery
+      -> JQuery
+      -> JQueryEvent
+      -> JQuery
+      -> Eff ( dom :: DOM
+             , console :: CONSOLE
+             | eff
+             ) Unit
+    handleCommand input output _ _ = unsafePartial do
+      val <- getValue input
+      for_ (runExcept (read val)) \command -> do
+        outLine <- create "<p>"
+        setText ("~> " <> command) outLine
+        append outLine output
